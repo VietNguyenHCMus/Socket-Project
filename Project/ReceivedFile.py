0946c6cb4446e450752f8114e0eb4f152ac7d53e
hinam201 <namdeptraiktvn@gmail.com>
2023-12-07 16:01:03 +0700

    new

---
+++ Project/ReceivedFile.py
@@ -0,0 +1,31 @@
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("localhost", 8889))
+server.listen()
+
+client, addr = server.accept()
+
+file_name = client.recv(1024).decode()
+print(file_name)
+file_size = client.recv(1024).decode()
+print(file_size)
+
+file = open(file_name, "wb")
+
+file_byte = b""
+
+done = False
+
+while not done:
+    data = client.recv(1024)
+    if file_byte[-5:] == b"<END>":
+        done = True
+    else:
+        file_byte += data
+
+file.write(file_byte)
+
+file.close()
+client.close()
+server.close()
---
+++ Project/Sendfile.py
@@ -0,0 +1,22 @@
+﻿import os
+import socket
+
+# Tạo một socket của client
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Kết nối đến server qua địa chỉ và cổng
+server_address = ('localhost', 8889)
+client_socket.connect(server_address)
+
+file = open("filename", "rb")
+file_size = os.path.getsize("filename")
+
+client_socket.send("received_file".encode())
+client_socket.send(str(file_size).endcode())
+
+data = file.read()
+client_socket.sendall(data)
+client_socket.send(b"<END>")
+
+file.close()
+client_socket.close()
